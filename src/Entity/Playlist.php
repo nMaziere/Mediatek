de59581a8a7836813fb55ecd179af2422a212d3c
nMaziere <mazierenicolas44@gmail.com>
2026-04-28 02:04:33 +0200

    Nettoyer le code en suivant les indications de Sonarlint (ne nettoyer que les fichiers créés par le développeur).

--- src/Entity/Playlist.php
+++ src/Entity/Playlist.php
@@ -82,27 +82,25 @@ class Playlist
 
     public function removeFormation(Formation $formation): static
     {
-        if ($this->formations->removeElement($formation)) {
+        if ($this->formations->removeElement($formation && $formation->getPlaylist() === $this)) {
             // set the owning side to null (unless already changed)
-            if ($formation->getPlaylist() === $this) {
                 $formation->setPlaylist(null);
-            }
         }
-
         return $this;
     }
     
     /**
      * @return Collection<int, string>
      */
-    public function getCategoriesPlaylist() : Collection
+   public function getCategoriesPlaylist(): Collection
     {
         $categories = new ArrayCollection();
-        foreach($this->formations as $formation){
+        foreach ($this->formations as $formation) {
             $categoriesFormation = $formation->getCategories();
-            foreach($categoriesFormation as $categorieFormation)
-            if(!$categories->contains($categorieFormation->getName())){
-                $categories[] = $categorieFormation->getName();
+            foreach ($categoriesFormation as $categorieFormation) {
+                if (!$categories->contains($categorieFormation->getName())) {
+                    $categories[] = $categorieFormation->getName();
+                }
             }
         }
         return $categories;
